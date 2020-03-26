0ac22280ccef24de350dd12d40a3cb7b5c974180
Mark Featherston <mark@embeddedarm.com>
2020-03-26 11:18:04 -0700

    pc104: Added comments explaining pc104 calls

--- src/pc104.h
+++ src/pc104.h
@@ -3,9 +3,24 @@
 
 #include <stdint.h>
 
-void pc104_init(void);
+/*
+ * This sets up a mmap to the pc104 space.
+ * 0-0xFFFFF           IO
+ * 0x100000-0x1FFFFF   MEM
+ *
+ * This calls pc104_init()
+ * To use this option the code must be compiled with -marm.
+ */
 void *pc104_mmap_init();
 
+/* This must be run before any of the below pc104 calls */
+void pc104_init(void);
+
+/* These all directly access the kernel driver to create 8,
+ * 16-bit, and alt 16-bit accesses to the PC104 bus.
+ *
+ * All addresses are byte addresses, and 16-bit accesses must
+ * be on even addresses. */
 void pc104_io_8_write(uint16_t addr, uint8_t val);
 void pc104_io_16_write(uint16_t addr, uint16_t val);
 void pc104_io_16_alt_write(uint16_t addr, uint16_t val);
